0e7575f48fdd05399496a774e981437b6ea76524
sumagic <kangxuzju@163.com>
2020-06-13 00:21:19 +0800

    delete before

--- include/xsys/xcompile.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MACRO_XLOG_COMPILE_H__
-#define __MACRO_XLOG_COMPILE_H__
-
-#define ERROR_USE_FILENAME (0)
-#define ERROR_USE_LINE (0)
-#define USE_XLOG (0)
-
-
-#endif // __MACRO_XLOG_COMPILE_H__
